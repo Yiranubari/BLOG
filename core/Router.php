b4c525c4e9f06d271e25c3926b8fd566a5170bc5
Yiranubari <yiranubari4@gmail.com>
2026-02-14 07:59:26 +0100

    Refactor matchRoute method to improve parameter handling and ensure proper return of parameters

--- core/Router.php
+++ core/Router.php
@@ -22,11 +22,14 @@ class Router
         }
         $params = [];
         foreach ($routeSegments as $index => $routeSegment) {
-            if (str_starts_with($routeSegment,'{') && str_ends_with($routeSegment, '}')) {
+            if (str_starts_with($routeSegment, '{') && str_ends_with($routeSegment, '}')) {
                 $params[trim($routeSegment, '{}')] = $requestSegments[$index];
+            } else if ($routeSegment !== $requestSegments[$index]) {
+                return null;
             }
+        }
+        return $params;
     }
-
     protected function callAction(string $controller, string $action, array $params): string
     {
         $controllerClass = "App\\Controller\\$controller";
