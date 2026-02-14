20309d778cecf7adb64eaafbf08be9c6cb29e0d0
Yiranubari <yiranubari4@gmail.com>
2026-02-14 07:18:06 +0100

    Add composer.lock, installed.php, installed.json, and InstalledVersions.php for dependency management

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vikky/blog',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2de02980bc3bc5a93757dbf61e7c5271734938d0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vikky/blog' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2de02980bc3bc5a93757dbf61e7c5271734938d0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
